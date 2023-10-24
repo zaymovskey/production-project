6a832c742eb5f853d7aeb713b2d19d855aff7838
zaymovskey <zaymovskey@mail.com>
2023-10-24 20:55:34 +0300

    jest, classNames tests

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,7 +1,21 @@
-import { classNames } from 'shared/lib/classNames/classNames';
+import { classNames } from './classNames';
 
 describe('classNames', () => {
-  test('test', () => {
+  test('with only first param', () => {
     expect(classNames('someClass')).toBe('someClass');
+  });
+
+  test('with additional class', () => {
+    const expected = 'someClass class1 class2';
+    expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+  });
+
+  test('with two modes (true false)', () => {
+    const expected = 'someClass class1 class2 hover';
+    expect(classNames(
+      'someClass',
+      { hover: true, scroll: false },
+      ['class1', 'class2'])
+    ).toBe(expected);
   });
 });
