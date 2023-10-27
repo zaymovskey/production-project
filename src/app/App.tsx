f4f44feb1cfe0e6833ef18d916cc87569da4cc7b
zaymovskey <zaymovskey@mail.com>
2023-10-27 22:55:52 +0300

    fdsaf

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/AppRouter';
 import { Navbar } from 'widgets/Navbar';
 import { SideBar } from 'widgets/SideBar';
-import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 
 const App: FC = () => {
   const { theme } = useTheme();
@@ -15,9 +14,7 @@ const App: FC = () => {
       <Navbar />
       <div className="content-page">
         <SideBar />
-        <ErrorBoundary>
-          <AppRouter />
-        </ErrorBoundary>
+        <AppRouter />
       </div>
     </div>
   );
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,7 @@
-import React, { type FC, useEffect } from 'react';
+import React, { type FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const MainPage: FC = () => {
-  useEffect(() => {
-    throw new Error();
-  }, []);
-
   const { t } = useTranslation('main');
 
   return <div>{t('Главная')}</div>;
