46ad6a0a887769293ab13a287a2cf28b0d9f1387
zaymovskey <zaymovskey@mail.com>
2023-10-29 00:16:02 +0300

    Eslint strictNullChecks

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,14 +1,12 @@
 import React, { type FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button } from 'shared/ui/Button/Button';
 
 const AboutPage: FC = () => {
   const { t } = useTranslation('about');
 
   return (
     <div>
-      {/* {t('О сайте')} */}
-      <Button>Клик</Button>
+      {t('О сайте')}
     </div>);
 };
 
