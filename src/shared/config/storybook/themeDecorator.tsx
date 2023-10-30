5ab5f619beb4c4a0f54db39b122c353dbbd0e48d
zaymovskey <zaymovskey@mail.com>
2023-10-30 23:50:23 +0300

    fix

--- src/shared/config/storybook/themeDecorator.tsx
+++ src/shared/config/storybook/themeDecorator.tsx
@@ -1,13 +1,11 @@
-import { type EnumTheme, ThemeProvider } from 'app/providers/ThemeProvider';
+import { type EnumTheme } from 'app/providers/ThemeProvider';
 import type { Decorator } from '@storybook/react';
 
 export const themeDecorator: Decorator = (Story, context) => {
   const currentTheme: EnumTheme = context.globals.theme;
   return (
-    <ThemeProvider>
-      <div className={`app ${currentTheme}`}>
-        <Story />
-      </div>
-    </ThemeProvider>
+    <div className={`app ${currentTheme}`}>
+      <Story />
+    </div>
   );
 };
