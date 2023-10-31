36e57630735e3ea2062c72e110f2b94eafa21882
zaymovskey <zaymovskey@mail.com>
2023-10-31 22:06:18 +0300

    test ui ci

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -6,7 +6,8 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    'creevey'
   ],
   framework: {
     name: '@storybook/react-webpack5',
