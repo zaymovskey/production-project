a9e20c9c91406f4a95f49388e9771747cc08a576
zaymovskey <zaymovskey@mail.com>
2023-11-01 20:48:13 +0300

    fdsafsd

--- scripts/generate-visual-json-report.js
+++ scripts/generate-visual-json-report.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { promisify } = require('util');
 const { readdir, writeFile } = require('fs');
 const { join: joinPath, relative } = require('path');
