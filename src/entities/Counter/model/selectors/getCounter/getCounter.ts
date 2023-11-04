49a55580bf585db27c2adba0fbd7c21293ab1127
zaymovskey <zaymovskey@mail.com>
2023-11-04 20:14:04 +0300

    fix

--- src/entities/Counter/model/selectors/getCounter/getCounter.ts
+++ src/entities/Counter/model/selectors/getCounter/getCounter.ts
@@ -1,3 +1,4 @@
 import { type StateScheme } from 'app/providers/StoreProvider';
+import { type CounterScheme } from 'entities/Counter';
 
-export const getCounter = (state: StateScheme) => state.counter;
+export const getCounter = (state: StateScheme): CounterScheme => state.counter;
