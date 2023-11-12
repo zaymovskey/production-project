db5a748d9771118ac56202de9690d009a3b731df
zaymovskey <zaymovskey@mail.com>
2023-11-12 18:45:40 +0300

    доработка модалки авторизации

--- src/features/Auth/ui/LoginModal/LoginModal.tsx
+++ src/features/Auth/ui/LoginModal/LoginModal.tsx
@@ -2,6 +2,7 @@ import { type FC, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { LoginForm } from 'features/Auth/ui/LoginForm/LoginForm';
 import { RegistrationForm } from 'features/Auth/ui/RegistrationForm/RegistrationForm';
+import AccountIcon from 'shared/assets/icons/account.svg';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { type IModalProps, Modal } from 'shared/ui/Modal/Modal';
 import { type ITab, Tabs } from 'shared/ui/Tabs/Tabs';
@@ -35,6 +36,9 @@ export const LoginModal: FC<ILoginModalProps> = ({ className, ...modalProps }) =
     <Modal className={classNames(cls.LoginModal, {}, [className])} {...modalProps}>
       <Tabs selectedId={selectedTabId} tabs={tabs} onClick={changeTabHandler} />
       <div className={cls.form}>
+        <div className={cls.accountIcon}>
+          <AccountIcon />
+        </div>
         {selectedTabId === 1 && <LoginForm />}
         {selectedTabId === 2 && <RegistrationForm />}
       </div>
