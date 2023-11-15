db1d8c81cd8436c2cf27d17d3874d2f92b8d1ffe
zaymovskey <zaymovskey@mail.com>
2023-11-15 19:04:59 +0300

    изменения переводов

--- config/i18n/i18n.ts
+++ config/i18n/i18n.ts
@@ -3,11 +3,24 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
+/* eslint-disable import/no-internal-modules */
+import translationEN from '../../public/locales/en.json';
+import translationRU from '../../public/locales/ru.json';
+/* eslint-enable import/no-internal-modules */
+
+console.log(translationEN);
+
+const resources = {
+  en: { translation: translationEN },
+  ru: { translation: translationRU }
+};
+
 void i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources,
     fallbackLng: 'ru',
     debug: _IS_DEV_,
 
